56951f386dbdbb3a4cd2b9d70ae3c6bb7205f13a
JulioCSimoesP <juliocsimoesp@gmail.com>
2023-07-20 16:08:49 -0300

    Gitignore ajustado

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'juliocsimoesp/php-pdo',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '46b1fccdc4e8d8fd2d06b822ba2bd5d1d63b15ac',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'juliocsimoesp/php-pdo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '46b1fccdc4e8d8fd2d06b822ba2bd5d1d63b15ac',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
